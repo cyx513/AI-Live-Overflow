504dab00de41ac90e40e25188acf6e9122dacf79
cyx513 <cyx2008513@qq.com>
2026-07-31 17:16:35 +0800

    Add MainActivity.kt

---
+++ app/src/main/java/com/bunnyoverflow/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.bunnyoverflow
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.provider.Settings
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (!Settings.canDrawOverlays(this)) {
+            val intent = Intent(
+                Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                Uri.parse("package:$packageName")
+            )
+            startActivity(intent)
+        }
+
+        startService(Intent(this, OverlayService::class.java))
+        finish()
+    }
+}
